8807a1cbe57cdd12305e48ee1250af5d92c90ec1
Burra Siva Kumar <40349298+sivakumar1998@users.noreply.github.com>
2025-03-12 19:46:01 +0530

    commented Test.java

--- auth/src/main/java/in/cadac/auth/auth/Test.java
+++ auth/src/main/java/in/cadac/auth/auth/Test.java
@@ -1,12 +1,12 @@
-package in.cadac.auth.auth;
-
-public class Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-String info="04{01000462LDBlGYLkB6ckGdbctEIz2fXctUeWQGxOBba3I3fkMayjqavufI4EG8CeQot8dhqP,A,5da986e8159427c4c7892374c54ba3bcf4fec4020297495152f0ecd3a32c8b41,0180000008000010,2.0,20250301154224,0,0,0,0,2.5,acf49307761427f68f53f156b4e72a444885a7646047fd11cdaaed88bbbb97aa,275ee032c0b98c3264761e606dd1ab8e43b638ec15dcf51c698a4c1f7eded2d5,275ee032c0b98c3264761e606dd1ab8e43b638ec15dcf51c698a4c1f7eded2d5,23,E,100,NA,NA,NA,NA,NA,NA,NA,,NA,NA,NA,NA,NA,NA}";
-for(String s:info.split("\\{"))
-System.out.println(s);
-	}
-
-}
+//package in.cadac.auth.auth;
+//
+//public class Test {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//String info="04{01000462LDBlGYLkB6ckGdbctEIz2fXctUeWQGxOBba3I3fkMayjqavufI4EG8CeQot8dhqP,A,5da986e8159427c4c7892374c54ba3bcf4fec4020297495152f0ecd3a32c8b41,0180000008000010,2.0,20250301154224,0,0,0,0,2.5,acf49307761427f68f53f156b4e72a444885a7646047fd11cdaaed88bbbb97aa,275ee032c0b98c3264761e606dd1ab8e43b638ec15dcf51c698a4c1f7eded2d5,275ee032c0b98c3264761e606dd1ab8e43b638ec15dcf51c698a4c1f7eded2d5,23,E,100,NA,NA,NA,NA,NA,NA,NA,,NA,NA,NA,NA,NA,NA}";
+//for(String s:info.split("\\{"))
+//System.out.println(s);
+//	}
+//
+//}
